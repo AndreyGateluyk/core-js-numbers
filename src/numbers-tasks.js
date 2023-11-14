700e9ad1018cbf0045fbc8dcd2e698a0ae29a4ce
Andrey Gateluyk <gatelyuk97@mail.ru>
2023-11-15 00:20:48 +0300

    add solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -198,8 +198,13 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -232,8 +237,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -249,8 +254,15 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(n) {
+  const arr = [0, 1];
+  if (n === 0) {
+    return 0;
+  }
+  for (let i = 1; i <= n - 1; i += 1) {
+    arr.push(arr[i - 1] + arr[i]);
+  }
+  return arr[arr.length - 1];
 }
 
 /**
@@ -264,8 +276,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i += 1) {
+    count += i;
+  }
+  return count;
 }
 
 /**
@@ -294,8 +310,12 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  const a = Math.log(num) / Math.log(2);
+  if (a % 1 === 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -308,8 +328,8 @@ function isPowerOfTwo(/* num */) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(n) {
+  return Math.sin(n);
 }
 
 /**
